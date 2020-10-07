28faf536278c47d92356d0777138c933bc4a0a8b
Christopher <colson16@asu.edu>
2020-10-07 11:43:22 -0700

    Initial Push
    Initial push for CSE360 assignment2 project.  Contains the AddingMachine.java and package

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,37 @@
+package cse360assignment02;
+
+/**
+ * 
+ * author: Christopher Olson
+ * class: CSE360
+ * proj: Assignment2
+ * Desc: AddingMachine class that contains the constructor
+ * 		and methods for the AddingMachine.  Keeps a record
+ * 		of inputs and saves them to a string.
+ *
+ **/
+
+public class AddingMachine {
+	  private int total;
+	  
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  public void add (int value) {
+	  }
+
+	  public void subtract (int value) {
+	  }
+
+	  public String toString () {
+	    return "";
+	  }
+
+	  public void clear() {
+	  }
+	}
